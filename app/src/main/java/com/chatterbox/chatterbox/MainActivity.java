d7d82a6549b6931600cf65d1558df8c9c11e04b9
Brian Lusina <lusinabrian@gmail.com>
2016-08-13 09:10:12 +0300

    Update gradle to add firebase dependency and tests

--- app/src/main/java/com/chatterbox/chatterbox/MainActivity.java
+++ app/src/main/java/com/chatterbox/chatterbox/MainActivity.java
@@ -1,5 +1,7 @@
 package com.chatterbox.chatterbox;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -17,6 +19,11 @@ public class MainActivity extends AppCompatActivity{
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainactivity_layout);
+    }
 
-/*CLASS END*/
+    /*CLASS END*/
 }
