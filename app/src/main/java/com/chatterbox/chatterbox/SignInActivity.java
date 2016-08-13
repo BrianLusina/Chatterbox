eeeadbe579770970a2fc627a20dbfa8b117f32fb
Brian Lusina <lusinabrian@gmail.com>
2016-08-13 08:24:57 +0300

    Add Sign in layout

--- app/src/main/java/com/chatterbox/chatterbox/SignInActivity.java
+++ app/src/main/java/com/chatterbox/chatterbox/SignInActivity.java
@@ -1,5 +1,7 @@
 package com.chatterbox.chatterbox;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -7,7 +9,19 @@ import android.support.v7.app.AppCompatActivity;
  * Package: com.chatterbox.chatterbox
  * Created by lusinabrian on 13/08/16 at 08:13
  * <p/>
- * Description:
+ * Description: sign in activity to authorize user to application
  */
 public class SignInActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signin_layout);
+
+        initUICtrls();
+    }
+    /*initialize the user controls*/
+    public void initUICtrls(){
+
+    }
+/*CLASS END*/
 }
