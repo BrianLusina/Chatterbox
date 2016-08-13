4d464824b648d08d48db14ba65ed2c2576f27664
Brian Lusina <lusinabrian@gmail.com>
2016-08-13 09:27:15 +0300

    Add messageViewHolder
    Handles the views of the messages received in a RecyclerView

--- app/src/main/java/com/chatterbox/chatterbox/MainActivity.java
+++ app/src/main/java/com/chatterbox/chatterbox/MainActivity.java
@@ -1,11 +1,19 @@
 package com.chatterbox.chatterbox;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Project: ChatterBox
@@ -14,10 +22,25 @@ import com.google.firebase.auth.FirebaseUser;
  * <p/>
  * Description:
  */
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
+
+    public static class MessageViewHolder extends RecyclerView.ViewHolder {
+        public TextView messageTextView;
+        public TextView messengerTextView;
+        public CircleImageView messengerImageView;
+
+        public MessageViewHolder(View v) {
+            super(v);
+            messageTextView = (TextView) itemView.findViewById(R.id.messageTextView);
+            messengerTextView = (TextView) itemView.findViewById(R.id.messengerTextView);
+            messengerImageView = (CircleImageView) itemView.findViewById(R.id.messengerImageView);
+        }
+    }
+
     // Firebase instance variables
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
+    private static final String MAINACTIVITY_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,5 +48,10 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.mainactivity_layout);
     }
 
-    /*CLASS END*/
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+/*CLASS END*/
 }
