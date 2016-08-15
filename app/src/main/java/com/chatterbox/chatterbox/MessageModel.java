415f98ed5c302cbbc13afae729201cc3f796d311
Brian Lusina <lusinabrian@gmail.com>
2016-08-15 15:42:02 +0300

    Create Message Model

---
+++ app/src/main/java/com/chatterbox/chatterbox/MessageModel.java
@@ -0,0 +1,56 @@
+package com.chatterbox.chatterbox;
+
+/**
+ * Project: ChatterBox
+ * Package: com.chatterbox.chatterbox
+ * Created by lusinabrian on 15/08/16 at 15:39
+ * <p/>
+ * Description: Properties of the message model displaying incoming messages
+ */
+public class MessageModel {
+    private String id, name, photoUrl,text;
+
+    /*constructor*/
+    public MessageModel(){}
+
+    public MessageModel(String name, String photoUrl, String text) {
+        this.name = name;
+        this.photoUrl = photoUrl;
+        this.text = text;
+    }
+
+    /*GETTERS AND SETTERS*/
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    /*CLASS END*/
+}
