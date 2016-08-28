d945ef35636dbeb002ce06575c2b14303a035def
Brian Lusina <lusinabrian@gmail.com>
2016-08-28 19:34:40 +0300

    Add reset password layout

--- app/src/main/java/com/chatterbox/chatterbox/login_signup/ResetPasswordActivity.java
+++ app/src/main/java/com/chatterbox/chatterbox/login_signup/ResetPasswordActivity.java
@@ -1,6 +1,9 @@
 package com.chatterbox.chatterbox.login_signup;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.chatterbox.chatterbox.R;
 
 /**
  * Project: ChatterBox
@@ -11,4 +14,9 @@ import android.support.v7.app.AppCompatActivity;
  */
 public class ResetPasswordActivity extends AppCompatActivity{
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.resetpassword_activity);
+    }
 }
