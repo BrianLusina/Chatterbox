e1c1f256f8f60f798e1e5d6d9e5731c6ca407dc5
Brian Lusina <lusinabrian@gmail.com>
2016-08-29 15:13:58 +0300

    Use LinearLayout as root layout
    Changed from CardView to LinearLayout in Login Fragment and SignUp Fragment

--- app/src/main/java/com/chatterbox/chatterbox/login_signup/ResetPasswordActivity.java
+++ app/src/main/java/com/chatterbox/chatterbox/login_signup/ResetPasswordActivity.java
@@ -39,7 +39,6 @@ public class ResetPasswordActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.resetpassword_activity);
-        super.onCreate(savedInstanceState);
 
         inputEmail = (EditText) findViewById(R.id.resetpass_email_id);
         btnReset = (Button) findViewById(R.id.resetpass_resetbtn_id);
