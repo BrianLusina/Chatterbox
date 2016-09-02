13ecda4ee6add9f7f21c53e79690dc6e429573d6
Brian Lusina <lusinabrian@gmail.com>
2016-09-02 16:37:11 +0300

    Add Chats frag item layout

---
+++ app/src/main/java/com/chatterbox/chatterbox/adapters/ChatsFragAdapter.java
@@ -0,0 +1,12 @@
+package com.chatterbox.chatterbox.adapters;
+
+/**
+ * Project: ChatterBox
+ * Package: com.chatterbox.chatterbox.adapters
+ * Created by lusinabrian on 02/09/16 at 16:16
+ * <p/>
+ * Description: Adapter that populates the ChatsFragmentRecyclerView
+ */
+public class ChatsFragAdapter {
+
+}
