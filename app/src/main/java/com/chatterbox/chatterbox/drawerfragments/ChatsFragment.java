c16588c585a83775ebf0c2d5987430f074758fdb
Brian Lusina <lusinabrian@gmail.com>
2016-09-02 12:22:01 +0300

    Create Chats fragment layout

--- app/src/main/java/com/chatterbox/chatterbox/drawerfragments/ChatsFragment.java
+++ app/src/main/java/com/chatterbox/chatterbox/drawerfragments/ChatsFragment.java
@@ -1,6 +1,14 @@
 package com.chatterbox.chatterbox.drawerfragments;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.chatterbox.chatterbox.R;
 
 /**
  * Project: ChatterBox
@@ -21,4 +29,34 @@ public class ChatsFragment extends Fragment{
         return chatsFragment;
     }
 
+    /**
+     * Called when the fragment's activity has been created and this
+     * fragment's view hierarchy instantiated.  It can be used to do final
+     * initialization once these pieces are in place, such as retrieving
+     * views or restoring state.  It is also useful for fragments that use
+     * {@link #setRetainInstance(boolean)} to retain their instance,
+     * as this callback tells the fragment when it is fully associated with
+     * the new activity instance.  This is called after {@link #onCreateView}
+     * and before {@link #onViewStateRestored(Bundle)}.
+     *
+     * @param savedInstanceState If the fragment is being re-created from
+     *                           a previous saved state, this is the state.
+     */
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.chatsfrag_layout, container,false);
+
+        return rootView;
+    }
 }
