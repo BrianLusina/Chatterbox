0ee9feb8747696e4b3fbb9ed97a4eb597c89f28d
Brian Lusina <lusinabrian@gmail.com>
2017-06-27 20:10:09 +0300

    update dependencies
    update dependencies and update gradle file to source sets for kotlin and java
    create strings in build file for manfest metadata, enables more security of application keys

--- app/src/main/java/com/chatterbox/chatterbox/app/ChatterBoxApp.java
+++ app/src/main/java/com/chatterbox/chatterbox/app/ChatterBoxApp.java
@@ -9,9 +9,7 @@ import com.chatterbox.chatterbox.di.components.DaggerAppComponent;
 import com.chatterbox.chatterbox.di.modules.AppModule;
 import com.crashlytics.android.Crashlytics;
 import com.crashlytics.android.answers.Answers;
-
 import javax.inject.Inject;
-import cat.ereza.customactivityoncrash.CustomActivityOnCrash;
 import io.fabric.sdk.android.Fabric;
 
 /**
@@ -54,29 +52,5 @@ public class ChatterBoxApp extends Application{
 
     public void setComponent(AppComponent mAppComponent){
         this.mAppComponent = mAppComponent;
-    }
-
-    /**
-     * Install custom crash to remove ANRs, because no one likes them :D
-     * This will not display a logcat in production/release builds only in debug builds
-     */
-    private void installCustomCrash() {
-
-        // do not start this error launcher when in the background
-        CustomActivityOnCrash.setLaunchErrorActivityWhenInBackground(false);
-
-        // set the default icon
-        CustomActivityOnCrash.setDefaultErrorActivityDrawable(R.mipmap.ic_launcher);
-
-        // setting the activity to start when the application crashes
-        // CustomActivityOnCrash.setRestartActivityClass(MainActivity.class);
-
-        //install the Custom Activity on crash
-        CustomActivityOnCrash.install(this);
-        CustomActivityOnCrash.setShowErrorDetails(BuildConfig.DEBUG);
-        CustomActivityOnCrash.setEnableAppRestart(true);
-
-        // initialize AuthConfig
-        //Fabric.with(this, new Twitter(authConfig));
     }
 }
